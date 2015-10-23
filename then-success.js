b5b45615b256502cce44af320bcc6f57db0018f7
x-bird <x-bird@qiubs.com>
2015-10-23 20:37:51 +0800

    maybe defer can be easier to learn

--- then-success.js
+++ then-success.js
@@ -6,53 +6,100 @@
 
 "use strict";
 
-var Promise = function(resolver) {
+var defer = function() {
+    var _pending = [], _value;
 
-    this._value;
-    this._reason;
-    this.pending = [];
+    return {
+        resolve: function(value) {
+            _value = value;
 
+            for(var i=0, ii = _pending.length; i < ii; ++i) {
+                _pending[i](_value);
+            }
 
-    resolver(this.resolve, this.reject);
+            _pending = undefined;
 
+        },
+        reject: function(){},
+        promise: {
+            then: function(callback){
+                if (_pending) {
+                    _pending.push(callback);
+                }
+            },
+            fail: function(){}
+        }
+    };
 }
 
-Promise.prototype.resolve = function(value) {
-    console.log('asdf', this);
-    console.log(this.pending);
-    this._value = value;
-    for (var i = 0, ii = this.pending.length; i < ii; i++) {
-        this.pending[i](this._value);
-    }
-    this.pending = undefined;
-};
+var a = function() {
+    var result = defer();
 
-Promise.prototype.reject = function(reason) {
-    this._reason = reason;
-};
-
-Promise.prototype.then = function(callback) {
-    // return {}
-    if (this.pending) {
-        this.pending.push(callback);
-    } else {
-        callback(_value);
-    }
-
-    // console.log(this);
-}
-
-var a = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        resolve('done')
+    setTimeout(function(){
+        result.resolve('times out');
     }, 1000);
+
+    return result.promise;
+};
+
+a().then(function(d) {
+    console.log('1', d);
 })
-a.then(function(d) {
-    console.log(d);
+a().then(function(d) {
+    console.log('2', d);
 })
-a.then(function(d) {
-    console.log(d);
+a().then(function(d) {
+    console.log('3', d);
 })
-a.then(function(d) {
-    console.log(d);
-})
+
+
+// var Promise = function(resolver) {
+
+//     this._value;
+//     this._reason;
+//     this.pending = [];
+
+
+//     resolver(this.resolve, this.reject);
+
+// }
+
+// Promise.prototype.resolve = function(value) {
+//     console.log('asdf', this);
+//     console.log(this.pending);
+//     this._value = value;
+//     for (var i = 0, ii = this.pending.length; i < ii; i++) {
+//         this.pending[i](this._value);
+//     }
+//     this.pending = undefined;
+// };
+
+// Promise.prototype.reject = function(reason) {
+//     this._reason = reason;
+// };
+
+// Promise.prototype.then = function(callback) {
+//     // return {}
+//     if (this.pending) {
+//         this.pending.push(callback);
+//     } else {
+//         callback(_value);
+//     }
+
+//     // console.log(this);
+// }
+
+// var a = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         resolve('done')
+//     }, 1000);
+// })
+// a.then(function(d) {
+//     console.log(d);
+// })
+// a.then(function(d) {
+//     console.log(d);
+// })
+// a.then(function(d) {
+//     console.log(d);
+// })
