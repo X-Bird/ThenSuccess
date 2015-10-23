15da1b4e26e3937a27a22bebe791b38d33df5855
x-bird <x-bird@qiubs.com>
2015-10-23 21:09:53 +0800

    finally done something...

--- then-success.js
+++ then-success.js
@@ -6,31 +6,72 @@
 
 "use strict";
 
+var wrap = function(value) {
+
+    // 有then方法就认为是个promise
+    if (value && typeof value.then === 'function') {
+        return value;
+    }
+
+    return {
+        then: function(callback) {
+            return wrap(callback(value));
+        }
+    }
+}
+
 var defer = function() {
     var _pending = [], _value;
 
     return {
         resolve: function(value) {
-            _value = value;
 
-            for(var i=0, ii = _pending.length; i < ii; ++i) {
-                _pending[i](_value);
+            if (_pending) {
+                _value = wrap(value); 
+                for (var i = 0, ii = _pending.length; i < ii; i++) {
+                    _value.then(_pending[i]);
+                }
+                _pending = undefined;
             }
+            // _value = value;
 
-            _pending = undefined;
+            // for(var i=0, ii = _pending.length; i < ii; ++i) {
+            //     _pending[i](_value);
+            // }
+
+            // _pending = undefined;
 
         },
         reject: function(){},
         promise: {
+            // 为了实现级联，需要 
+            // 1. then 返回一个promise， 
+            // 2. 这个promise的then方法的callback方法的value来自于上个promise的resovle出来的结果
+            // 3. （这点不是自己想到的，是看文档的） 传递给 then 的callback 需要返回一个 promise 或者是 一个值
             then: function(callback){
+
+                var result = defer();
+
+                var _wrappedCallback = function(_value) {
+                    result.resolve(callback(_value));
+                };
+
                 if (_pending) {
-                    _pending.push(callback);
+                    _pending.push(_wrappedCallback);
                 }
+                else {
+                    _value.then(_wrappedCallback(_value));
+                }
+
+                return result.promise;
             },
             fail: function(){}
         }
     };
 }
+
+
+// 测试用例
 
 var a = function() {
     var result = defer();
@@ -52,6 +93,14 @@ a().then(function(d) {
     console.log('3', d);
 })
 
+a()
+.then(function(d){ console.log('10', d); return '10'; })
+.then(function(d){ console.log('9', d); return '9'; })
+.then(function(d){ console.log('7', d); return '7'; })
+.then(function(d){ console.log('8', d); return '8'; })
+.then(function(d){ console.log('11', d); return '11'; })
+
+// 另一种实现
 
 // var Promise = function(resolver) {
 
